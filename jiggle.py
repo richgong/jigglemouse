30dccf32d782bde0d8c257794b1c1f454f6ecf23
Richard Gong <richard.gong@instacart.com>
2022-03-05 21:23:04 -0800

    click

--- jiggle.py
+++ jiggle.py
@@ -5,8 +5,6 @@ from pynput.mouse import Button, Controller
 mouse = Controller()
 
 # Read pointer position
-print('The current pointer position is {0}'.format(
-    mouse.position))
 
 # # Press and release
 # mouse.press(Button.left)
@@ -24,6 +22,12 @@ base = 100
 while True:
     rand = random.random() * 1.0
     print(rand)
+    print(f'Current position: {int(mouse.position[0])} {int(mouse.position[1])} rand={rand}')
+    mouse.position = (549, 637)
+    mouse.click(Button.left, 1)
+    time.sleep(rand * 2.0 + 1.0)
+    mouse.position = (1214, 637)
+    mouse.click(Button.left, 1)
     time.sleep(rand)
     mouse.move(base * rand, base * rand)
     time.sleep(rand)
